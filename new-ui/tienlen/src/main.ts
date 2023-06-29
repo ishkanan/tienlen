b8ad7293b7d2984bdc3bada7d7fd3fcaed5ea356
ishkanan <anthony.ishkan@gmail.com>
2023-06-29 22:38:10 +1000

    Pipeline and bootstrap migrations

--- new-ui/tienlen/src/main.ts
+++ new-ui/tienlen/src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import './assets/css/fonts.css'
+import './assets/css/global.css'
 
 import { createApp } from 'vue'
+import Toasted from 'vue-toasted'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Toasted)
 
 app.mount('#app')
