a0fa6fd0e5f69aa12402e0649d6d346aefb9fc07
ishkanan <anthony.ishkan@gmail.com>
2023-07-03 22:04:08 +1000

    Split card assets

--- split_ui_assets.py
+++ split_ui_assets.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
+
 import cv2
 
 
-def generate_tiles(image_path, out_folder, step_width, step_height):
+def rune_rank_gen():
+    ranks = \
+        [15, 14, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16] + \
+        [2, 1, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3] + \
+        [41, 40, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42] + \
+        [28, 27, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29]
+    for rank in ranks:
+        yield rank
+    raise StopIteration
+
+
+def card_rank_gen():
+    ranks = \
+        ['-back-black', 28, 27, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29] + \
+        ['-back-red', 15, 14, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16] + \
+        [0, 2, 1, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3] + \
+        [0, 41, 40, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42]
+    for rank in ranks:
+        yield rank
+    raise StopIteration
+
+
+def generate_tiles(image_path, out_folder, step_width, step_height, rank_gen):
+    shutil.rmtree(out_folder, True)
+    os.mkdir(out_folder)
+
     img = cv2.imread(image_path)
     height = img.shape[0]
     width = img.shape[1]
@@ -11,7 +39,6 @@ def generate_tiles(image_path, out_folder, step_width, step_height):
 
     x1 = 0
     y1 = 0
-    rank = 52
 
     for y in range(0, height, step_height):
         for x in range(0, width, step_width):
@@ -21,7 +48,7 @@ def generate_tiles(image_path, out_folder, step_width, step_height):
             y1 = y + step_height
             x1 = x + step_width
             tiles = None
-            rank += 1
+            rank = next(rank_gen)
 
             # check whether the patch width or height exceeds the image width or height
             if x1 >= width and y1 >= height:
@@ -37,14 +64,15 @@ def generate_tiles(image_path, out_folder, step_width, step_height):
             else:
                 tiles = data[y:y + step_height, x:x + step_width]
 
-            cv2.imwrite(f'{out_folder}/tile{rank}.png', tiles)
+            out_file = f'{out_folder}/tile{rank}.png'
+            cv2.imwrite(out_file, tiles)
             cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0), 1)
-
-# TODO: update the PNGs to make these work
+            print(f'> created {out_file}')
 
 
 print('Generating tiles for cards...')
-generate_tiles('ui/src/assets/images/cards.png', 'ui/src/assets/images/cards', 0, 0)
+generate_tiles('ui/src/assets/images/cards.png', 'ui/src/assets/images/cards', 80, 120, card_rank_gen())
+os.remove('ui/src/assets/images/cards/tile0.png')
 
 print('Generating tiles for card runes...')
-generate_tiles('ui/src/assets/images/cards-runes.png', 'ui/src/assets/images/card-runes', 0, 0)
+generate_tiles('ui/src/assets/images/cards-runes.png', 'ui/src/assets/images/card-runes', 21, 32, rune_rank_gen())
--- ui/src/components/Card.vue
+++ ui/src/components/Card.vue
@@ -14,29 +14,7 @@ const emit = defineEmits<{
 
 const selected = ref(false)
 
-const suitYmap: Record<Suit, number> = {
-  [Suit.Clubs]: 0,
-  [Suit.Diamonds]: 120,
-  [Suit.Hearts]: 240,
-  [Suit.Spades]: 360,
-}
-
-const offsetX = computed(() => {
-  if (!props.showFace) return 0
-  return 80 * props.card.faceValue
-})
-
-const offsetY = computed(() => {
-  if (!props.showFace) return [Suit.Spades, Suit.Clubs].includes(props.card.suit) ? 0 : 120
-  return suitYmap[props.card.suit]
-})
-
-const style = computed<Record<string, string>>(() => {
-  return {
-    cursor: props.selectable ? 'pointer' : 'default',
-    backgroundPosition: `top -${offsetY.value}px left -${offsetX.value}px`,
-  }
-})
+const redCard = computed(() => [Suit.Hearts, Suit.Diamonds].includes(props.card.suit))
 
 const onClick = () => {
   if (!props.selectable) return
@@ -46,15 +24,191 @@ const onClick = () => {
 </script>
 
 <template>
-  <div class="card" :style="style" @click="onClick">
+  <div
+    class="tile"
+    :class="{
+      selectable: props.selectable,
+      [`tile${card.globalRank}`]: props.showFace,
+      [`tile-back-red`]: !props.showFace && redCard,
+      [`tile-back-black`]: !props.showFace && !redCard,
+    }" @click="onClick">
     <slot name="info" />
   </div>
 </template>
 
 <style scoped>
-.card {
+.tile {
+  cursor: default;
   width: 80px;
   height: 120px;
-  background: url(../assets/images/cards.png);
+  max-width: 80px;
+  max-height: 120px;
+}
+
+.selectable {
+  cursor: pointer;
+}
+
+.tile-back-red {
+  background: url(../assets/images/cards/tile-back-red.png);
+}
+.tile-back-black {
+  background: url(../assets/images/cards/tile-back-black.png);
+}
+.tile1 {
+  background: url(../assets/images/cards/tile1.png);
+}
+.tile2 {
+  background: url(../assets/images/cards/tile2.png);
+}
+.tile3 {
+  background: url(../assets/images/cards/tile3.png);
+}
+.tile4 {
+  background: url(../assets/images/cards/tile4.png);
+}
+.tile5 {
+  background: url(../assets/images/cards/tile5.png);
+}
+.tile6 {
+  background: url(../assets/images/cards/tile6.png);
+}
+.tile7 {
+  background: url(../assets/images/cards/tile7.png);
+}
+.tile8 {
+  background: url(../assets/images/cards/tile8.png);
+}
+.tile9 {
+  background: url(../assets/images/cards/tile9.png);
+}
+.tile10 {
+  background: url(../assets/images/cards/tile10.png);
+}
+.tile11 {
+  background: url(../assets/images/cards/tile11.png);
+}
+.tile12 {
+  background: url(../assets/images/cards/tile12.png);
+}
+.tile13 {
+  background: url(../assets/images/cards/tile13.png);
+}
+.tile14 {
+  background: url(../assets/images/cards/tile14.png);
+}
+.tile15 {
+  background: url(../assets/images/cards/tile15.png);
+}
+.tile16 {
+  background: url(../assets/images/cards/tile16.png);
+}
+.tile17 {
+  background: url(../assets/images/cards/tile17.png);
+}
+.tile18 {
+  background: url(../assets/images/cards/tile18.png);
+}
+.tile19 {
+  background: url(../assets/images/cards/tile19.png);
+}
+.tile20 {
+  background: url(../assets/images/cards/tile20.png);
+}
+.tile21 {
+  background: url(../assets/images/cards/tile21.png);
+}
+.tile22 {
+  background: url(../assets/images/cards/tile22.png);
+}
+.tile23 {
+  background: url(../assets/images/cards/tile23.png);
+}
+.tile24 {
+  background: url(../assets/images/cards/tile24.png);
+}
+.tile25 {
+  background: url(../assets/images/cards/tile25.png);
+}
+.tile26 {
+  background: url(../assets/images/cards/tile26.png);
+}
+.tile27 {
+  background: url(../assets/images/cards/tile27.png);
+}
+.tile28 {
+  background: url(../assets/images/cards/tile28.png);
+}
+.tile29 {
+  background: url(../assets/images/cards/tile29.png);
+}
+.tile30 {
+  background: url(../assets/images/cards/tile30.png);
+}
+.tile31 {
+  background: url(../assets/images/cards/tile31.png);
+}
+.tile32 {
+  background: url(../assets/images/cards/tile32.png);
+}
+.tile33 {
+  background: url(../assets/images/cards/tile33.png);
+}
+.tile34 {
+  background: url(../assets/images/cards/tile34.png);
+}
+.tile35 {
+  background: url(../assets/images/cards/tile35.png);
+}
+.tile36 {
+  background: url(../assets/images/cards/tile36.png);
+}
+.tile37 {
+  background: url(../assets/images/cards/tile37.png);
+}
+.tile38 {
+  background: url(../assets/images/cards/tile38.png);
+}
+.tile39 {
+  background: url(../assets/images/cards/tile39.png);
+}
+.tile40 {
+  background: url(../assets/images/cards/tile40.png);
+}
+.tile41 {
+  background: url(../assets/images/cards/tile41.png);
+}
+.tile42 {
+  background: url(../assets/images/cards/tile42.png);
+}
+.tile43 {
+  background: url(../assets/images/cards/tile43.png);
+}
+.tile44 {
+  background: url(../assets/images/cards/tile44.png);
+}
+.tile45 {
+  background: url(../assets/images/cards/tile45.png);
+}
+.tile46 {
+  background: url(../assets/images/cards/tile46.png);
+}
+.tile47 {
+  background: url(../assets/images/cards/tile47.png);
+}
+.tile48 {
+  background: url(../assets/images/cards/tile48.png);
+}
+.tile49 {
+  background: url(../assets/images/cards/tile49.png);
+}
+.tile50 {
+  background: url(../assets/images/cards/tile50.png);
+}
+.tile51 {
+  background: url(../assets/images/cards/tile51.png);
+}
+.tile52 {
+  background: url(../assets/images/cards/tile52.png);
 }
 </style>
